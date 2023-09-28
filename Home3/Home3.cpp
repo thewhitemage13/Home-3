e052b18721abbfaf5e3568c267f8257e0c9e32ca
thewhitemage13 <144810207+thewhitemage13@users.noreply.github.com>
2023-09-28 11:56:06 +0200

    Add files via upload

---
+++ Home3/Home3.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+void Sorting(double a[],int size)
+{
+	for (int i=1; i < 5; i++)
+	{
+		for (int j = 0; j < 5 - i; j++)
+		{
+			if (a[j] < a[j + 1])
+			{
+				int temp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = temp;
+			}
+		}
+	}
+
+	for (int i = 0; i < 5; i++)
+	{
+		cout << a[i];
+	}
+}
+
+int main()
+{
+	int size = 5;
+	double a[] = {1,2,3,4,5};
+
+	for (int i = 0; i < size; i++) 
+	{
+		cout << a[i];
+	}
+
+	cout << "\n";
+	Sorting(a, size);
+}
